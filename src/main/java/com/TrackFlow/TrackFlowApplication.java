d6ed32b3ba4c6d96f6f88990526b4d4b4fc3dd3f
tinoKim <gksmfwlsgh@nate.com>
2024-09-09 06:31:35 +0900

    Initial commit, PostgreSQL Docker Setting

---
+++ src/main/java/com/TrackFlow/TrackFlowApplication.java
@@ -0,0 +1,13 @@
+package com.TrackFlow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackFlowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackFlowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/TrackFlow/TrackFlowApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TrackFlow;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackFlowApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
